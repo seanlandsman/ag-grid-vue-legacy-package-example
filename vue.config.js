302c1030b03cd4d65265fba276ea4301a610e971
Sean Landsman <sean@thelandsmans.com>
2020-10-07 11:23:18 +0100

    First commit

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+    configureWebpack: {
+        resolve: {
+            symlinks: false,
+            alias: {
+                // Make sure *our* version of ag-grid & vue is always loaded.
+                // This is needed for `yarn link / npm link` to work and prevent duplicate versions of these libs
+                // being loaded
+                'ag-grid-community$': path.resolve(__dirname, 'node_modules/ag-grid-community'),
+                vue$: path.resolve(__dirname, 'node_modules/vue/dist/vue.common.js')
+            }
+        },
+        performance: {
+            hints: false
+        }
+    }
+};
